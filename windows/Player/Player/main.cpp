633437122bd151bf177c840e309d09e3a2c75ecf
Daniel Lindsäth <alkanen@gmail.com>
2016-04-30 01:13:26 +0200

    Fixed the colours and added up and down keys in order to make it a more general client.  Need to map the other four actions as well eventually.

--- windows/Player/Player/main.cpp
+++ windows/Player/Player/main.cpp
@@ -16,47 +16,49 @@ extern "C" {
 
 #define FLOAT_TO_PIXEL(__val__) \
 		( \
-			(((unsigned int)(__val__ * 0xff) & 0xff)) | \
+			(((unsigned int)(__val__ * 0xff) << 0) & 0xff) | \
 			(((unsigned int)(__val__ * 0xff) << 8) & 0xff00) | \
 			(((unsigned int)(__val__ * 0xff) << 16) & 0xff0000) | \
-			0xff000000 \
+			(((unsigned int)(__val__ * 0xff) << 24) & 0xff000000) \
 		)
 
 int main() {
 	srand((unsigned)(time(NULL)));
 
-	game_t *arkanoid = createArkanoid(-1);
+	game_t *game = createArkanoid(-1);
 	input_t inputs = { 0, };
 
-	Screen screen(arkanoid->screen_width, arkanoid->screen_height, "AI world");
+	Screen screen(game->screen_width, game->screen_height, "AI world");
 
 	int leftState = 0;
 	int rightState = 0;
 	int upState = 0;
 	int downState = 0;
 
-	while (GetAsyncKeyState(VK_ESCAPE) == 0 && arkanoid->game_over != true) {
+	while (GetAsyncKeyState(VK_ESCAPE) == 0 && game->game_over != true) {
 		leftState = GetAsyncKeyState(VK_LEFT);
 		rightState = GetAsyncKeyState(VK_RIGHT);
 
-		for (unsigned int y = 0; y < arkanoid->screen_height; y++) {
-			for (unsigned int x = 0; x < arkanoid->screen_width; x++) {
+		for (unsigned int y = 0; y < game->screen_height; y++) {
+			for (unsigned int x = 0; x < game->screen_width; x++) {
 				// Windows screens are upside down...
-				float val = arkanoid->screen[y * arkanoid->screen_width + x];
-				screen.pixels[(arkanoid->screen_height - y - 1) * arkanoid->screen_width + x] = FLOAT_TO_PIXEL(val * 0xff);
+				uint32_t gVal = 0xff * game->screen[y * game->screen_width + x];
+				uint32_t color = gVal | (gVal << 8) | (gVal << 16) | (gVal << 24);
+
+				screen.pixels[(game->screen_height - y - 1) * game->screen_width + x] = color;
 			}
 		}
 
 		// Draw new stuff
 		screen.swap();
 
-		inputs.left  = leftState ? 1 : 0;
-		inputs.right = rightState ? 1 : 0;
-		inputs.up    = upState ? 1 : 0;
-		inputs.down  = downState ? 1 : 0;
+		inputs.left  = (float)(leftState  ? 1.0 : 0.0);
+		inputs.right = (float)(rightState ? 1.0 : 0.0);
+		inputs.up    = (float)(upState    ? 1.0 : 0.0);
+		inputs.down  = (float)(downState  ? 1.0 : 0.0);
 
 		// Send input to game
-		arkanoid->_update(arkanoid, inputs);
+		game->_update(game, inputs);
 	}
 
 	return 0;
